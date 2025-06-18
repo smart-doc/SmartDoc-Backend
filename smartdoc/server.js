096d1d6366d86b7a65345241ef741ea00fdc5af4
Chumexxx <obasyemeka@gmail.com>
2025-06-18 12:45:56 +0100

    .

--- smartdoc/server.js
+++ smartdoc/server.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: function(origin, callback) {
-    const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+    const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'];
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
